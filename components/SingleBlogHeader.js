907759c59386542e2a050c38fdfd22c0e5ea5dfa
lakotelman <la.n.kot@pm.me>
2022-10-19 09:48:33 -0800

    fix banner on single post

--- components/SingleBlogHeader.js
+++ components/SingleBlogHeader.js
@@ -1,7 +1,8 @@
-import Link from "next/link";
+import bg from "../public/images/betterwaves.svg"
+
 export default function BlogSingleHeader(props) {
   return (
-    <div className="header-background-small">
+    <div className="header-background-small" style={{backgroundImage:`url(${bg.src})`}}>
       {props.children}
       
     </div>
